a3805be58ad92a3a63a230ebbc966783e2de29e4
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2014-10-06 00:44:48 +0200

    Document content output - basic implementation

--- tex_output.py
+++ tex_output.py
@@ -4,10 +4,91 @@ __license__ = 'MIT'
 __copyright__ = '2014, Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>'
 __docformat__ = 'restructuredtext en'
 
+from lxml import etree
 import os
 
 from calibre.customize.conversion import OptionRecommendation
 from calibre.customize.conversion import OutputFormatPlugin
+from calibre.ebooks.oeb.base import XPath
+from calibre.ebooks.oeb.base import XHTML
+
+
+class RecodeCallbackRegistry(type):
+
+    # registry of defined recode callbacks
+    registry = {}
+
+    def __init__(cls, name, bases, attrs):
+        RecodeCallbackRegistry.registry[cls.tag] = cls()
+        return type.__init__(cls, name, bases, attrs)
+
+    @classmethod
+    def get(cls, tag):
+        return cls.registry.get(tag)
+
+    @classmethod
+    def set_logger(cls, logger):
+        for callback in cls.registry.values():
+            callback.log = logger
+
+
+class RecodeCallbackBase:
+    __metaclass__ = RecodeCallbackRegistry
+
+    # NOTE: body is our first document structure tag, so it is safe to use
+    #       it as a callback base example
+    tag = XHTML('body')
+
+    # XXX: when callback entry point is called, it is guarantied that this
+    #      attribute is set to the current application logger
+    log = None
+
+    def start(self, element):
+        return self.get_text(element)
+
+    def end(self, element):
+        return self.get_tail(element)
+
+    @staticmethod
+    def get_text(element):
+        return element.text or ""
+
+    @staticmethod
+    def get_tail(element):
+        return element.tail or ""
+
+    @staticmethod
+    def get_classes(element):
+        return element.attrib.get('class', "").split()
+
+
+class RecodeCallbackP(RecodeCallbackBase):
+
+    tag = XHTML('p')
+
+    def end(self, element):
+        return "\n\n"
+
+
+class RecodeCallbackSpan(RecodeCallbackBase):
+
+    tag = XHTML('span')
+
+    def start(self, element):
+
+        # font format is encoded in the class attribute
+        classes = self.get_classes(element)
+
+        if 'bold' in classes:
+            return "\\textbf{" + self.get_text(element)
+        elif 'italic' in classes:
+            return "\\emph{" + self.get_text(element)
+
+        self.log.warning("unrecognized span class:", classes)
+        return "{" + self.get_text(element)
+
+    def end(self, element):
+        return "}" + self.get_tail(element)
 
 
 class LatexOutput(OutputFormatPlugin):
@@ -17,6 +98,16 @@ class LatexOutput(OutputFormatPlugin):
     file_type = 'tex'
 
     options = set([
+        OptionRecommendation(
+            name='latex_title_page',
+            recommended_value=True,
+            help=_('Insert Latex default Title Page which will appear as part of main book content.'),
+        ),
+        OptionRecommendation(
+            name='latex_toc',
+            recommended_value=False,
+            help=_('Insert Latex default Table of Contents which will appear as part of the main book content.'),
+        ),
     ])
 
     recommendations = set([
@@ -24,7 +115,8 @@ class LatexOutput(OutputFormatPlugin):
     ])
 
     def convert(self, oeb, output_path, input_plugin, opts, log):
-        self.opts, self.log = opts, log
+        self.oeb, self.opts, self.log = oeb, opts, log
+        RecodeCallbackRegistry.set_logger(log)
 
         # try to get basic metadata of this document
         authors = map(lambda x: x.value, oeb.metadata.author)
@@ -39,6 +131,8 @@ class LatexOutput(OutputFormatPlugin):
         output_dir = os.path.dirname(output_path)
         if not os.path.exists(output_dir) and output_dir:
             os.makedirs(output_dir)
+
+        #import pdb; pdb.set_trace()
 
         # open output file for content writing
         with open(output_path, 'w') as f:
@@ -84,11 +178,74 @@ class LatexOutput(OutputFormatPlugin):
             # write document content
             f.write((
                 "\\begin{{document}}\n"
-                "{content}\n"
+                "{titlepage}"  # this section has a new line on its own
+                "{tocpage}"    # this section has a new line on its own
+                "\n"
+                "{content}"    # this section has a new line on its own
+                "\n"
                 "\\end{{document}}\n"
             ).format(
-                content="",
+                titlepage=self.latex_format_titlepage(),
+                tocpage=self.latex_format_tocpage(),
+                content=self.latex_format_content(),
             ))
+
+    def latex_format_titlepage(self):
+        if not self.opts.latex_title_page:
+            return "% \\maketitle\n"
+        return "\\maketitle\n"
+
+    def latex_format_tocpage(self):
+        if not self.opts.latex_toc:
+            return "% \\tableofcontents\n"
+        return "\\tableofcontents\n"
+
+    def latex_format_content(self):
+
+        # collection of TeX document chunks
+        content = []
+
+        for x in self.oeb.spine:
+            try:
+                body = XPath('//h:body')(x.data)[0]
+            except IndexError:
+                continue
+            # recode the OEB document nodes into the TeX syntax
+            for event, element in etree.iterwalk(body, ('start', 'end')):
+                callback = RecodeCallbackRegistry.get(element.tag)
+                if not callback:
+                    self.log.warning("unhandled tag:", element.tag)
+                    continue
+                if event == 'start':
+                    content.append(callback.start(element))
+                elif event == 'end':
+                    content.append(callback.end(element))
+
+        content = "".join(content)
+
+        # content post-processing
+
+        if self.opts.pretty_print:
+            content = self.latex_pretty_print(content)
+
+        return content
+
+    @staticmethod
+    def latex_pretty_print(content, length=78):
+        lines = []
+        for line in content.splitlines():
+            while len(line) > length:
+                space = line.rfind(" ", 0, length)
+                if space == -1:
+                    # look for the first space after the length
+                    space = line.find(" ", length, len(line))
+                if space == -1:
+                    # we are too dumb to break this line
+                    break
+                lines.append(line[:space])
+                line = line[space + 1:]
+            lines.append(line)
+        return "\n".join(lines)
 
     @staticmethod
     def latex_convert_languages(languages):
